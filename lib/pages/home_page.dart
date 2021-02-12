6d034206ed5e74c7804ffb2e319a4003c75e6629
Efrain May <efrain_may_75@hotmail.com>
2021-02-12 10:58:19 -0600

    pantallas y rutas creadas

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+
+class HomePage extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Pagar'),
+      ),
+      body: Center(
+        child: Text('Hola Mundo'),
+      ),
+    );
+  }
+}

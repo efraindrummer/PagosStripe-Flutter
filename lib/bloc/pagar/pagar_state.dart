7c60b9cef03df70e9763e7ef1ea9a798a9dda016
Efrain May <efrain_may_75@hotmail.com>
2021-02-12 19:08:13 -0600

    patron bloc de tarjetas

---
+++ lib/bloc/pagar/pagar_state.dart
@@ -0,0 +1,30 @@
+part of 'pagar_bloc.dart';
+
+@immutable
+class PagarState{
+
+  final double montoPagar;
+  final String moneda;
+  final bool tarjetaActiva;
+  final TarjetaCredito tarjeta;
+
+  PagarState({
+    this.montoPagar = 735.55, 
+    this.moneda = 'USD', 
+    this.tarjetaActiva = false, 
+    this.tarjeta
+  });
+
+  PagarState copyWith({
+    double montoPagar,
+    String moneda,
+    bool tarjetaActiva,
+    TarjetaCredito tarjeta,
+  }) => PagarState(
+    montoPagar: montoPagar ?? this.montoPagar,
+    moneda    : moneda ?? this.moneda,
+    tarjeta   : tarjeta ?? this.tarjeta,
+    tarjetaActiva: tarjetaActiva ?? this.tarjetaActiva,
+  );
+
+}
